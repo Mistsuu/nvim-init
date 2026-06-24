f3917f1a5af0707d8f751c36b9a8fdb60c6caef7
Your Name <you@example.com>
2026-06-25 01:11:59 +0800

    chore: add configs from suggestions & better boxes

--- lua/plugins/noice.lua
+++ lua/plugins/noice.lua
@@ -29,13 +29,18 @@ end
 return {
 	"folke/noice.nvim",
 	event = "VeryLazy",
+  dependencies = {
+    "MunifTanjim/nui.nvim",
+    "hrsh7th/nvim-cmp",
+    "rcarriga/nvim-notify",
+  },
 	opts = {
-		-- views options
 		views = getviews(),
 		lsp = {
 			override = {
 				["vim.lsp.util.convert_input_to_markdown_lines"] = true,
 				["vim.lsp.util.stylize_markdown"] = true,
+        ["cmp.entry.get_documentation"] = true,
 			},
 			signature = {
 				enabled = false,
@@ -69,9 +74,10 @@ return {
 			},
 		},
 		presets = {
-			bottom_search = false,
+			bottom_search = true,
 			command_palette = true,
 			long_message_to_split = true,
+      inc_rename = false,
 			lsp_doc_border = true,
 		},
 	},
